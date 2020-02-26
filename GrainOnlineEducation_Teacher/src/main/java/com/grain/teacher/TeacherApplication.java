ef36ee377da194ef7eba6f7c397c84117e078eca
温文星 <18475536452@163.com>
2020-02-26 21:13:26 +0800

    谷粒学院在校教育项目构建

---
+++ GrainOnlineEducation_Teacher/src/main/java/com/grain/teacher/TeacherApplication.java
@@ -0,0 +1,20 @@
+package com.grain.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/2/26 18:50
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@SpringBootApplication
+public class TeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherApplication.class, args);
+    }
+}
