8b1d4349c0de986075797d5a4b3ac2c2964e82aa
温文星 <18475536452@163.com>
2020-03-13 22:18:39 +0800

    根据token信息jwt的令牌，获取用户信息返回

--- GrainOnlineEducation_Ucenter/src/main/java/com/grain/ucenter/controller/MemberController.java
+++ GrainOnlineEducation_Ucenter/src/main/java/com/grain/ucenter/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.grain.ucenter.controller;
 
 import com.grain.common.result.Result;
+import com.grain.ucenter.entity.Member;
 import com.grain.ucenter.service.MemberService;
+import com.grain.ucenter.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +33,24 @@ public class MemberController {
         @PathVariable String day){
         Integer count = memberService.registerCountNum(day);
         return Result.ok().data("countRegister", count);
+    }
+
+    //根据token信息jwt的令牌，获取用户信息返回
+    @ApiOperation(value = "根据token信息jwt的令牌，获取用户信息返回")
+    @GetMapping("userInfo/{token}")
+    public Result getUserInfoToken(@PathVariable String token) {
+        //调用工具类获取用户信息
+        Claims claims = JwtUtils.checkJWT(token);
+        String nickname = (String)claims.get("nickname");
+        String avatar = (String)claims.get("avatar");
+        String id = (String)claims.get("id");
+
+        Member member = new Member();
+        member.setId(id);
+        member.setId(avatar);
+        member.setNickname(nickname);
+
+        return Result.ok().data("member",member);
     }
 }
 
