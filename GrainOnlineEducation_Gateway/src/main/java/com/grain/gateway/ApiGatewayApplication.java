cb4033f6b013807c9d252eb43ce10a62f394356d
温文星 <18475536452@163.com>
2020-03-14 14:04:12 +0800

    配置网关服务化

--- GrainOnlineEducation_Gateway/src/main/java/com/grain/gateway/ApiGatewayApplication.java
+++ GrainOnlineEducation_Gateway/src/main/java/com/grain/gateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.grain.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
